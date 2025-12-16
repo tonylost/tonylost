5c08e53647f13b2648efd887640ee33badfcef47
Tony Lost <835437+tonylost@users.noreply.github.com>
2025-12-16 03:21:19 +0100

    Refactor service worker for improved caching
    Signed-off-by: Tony Lost <835437+tonylost@users.noreply.github.com>

--- ane/manual/gen/service-worker.js
+++ ane/manual/gen/service-worker.js
@@ -1,35 +1,18 @@
 // This is the "Offline copy of pages" service worker
-// service-worker.js
-importScripts('storage.googleapis.com');
 
-if (workbox.googleAnalytics) {
-  workbox.googleAnalytics.initialize();
-}
+const CACHE = "pwabuilder-offline";
+
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
+});
 
 workbox.routing.registerRoute(
-  // Cachea todos los archivos estáticos: CSS, JS, PNG, HTML, etc.
-  new RegExp('.*\.(?:png|jpg|jpeg|svg|gif|html|css|js)$'),
+  new RegExp('/*'),
   new workbox.strategies.StaleWhileRevalidate({
-    cacheName: 'mi-pwa-static-cache',
-    plugins: [
-      new workbox.expiration.ExpirationPlugin({
-        maxEntries: 50,
-        maxAgeSeconds: 7 * 24 * 60 * 60, // Cache for 1 week
-      }),
-    ],
+    cacheName: CACHE
   })
 );
-
-// Usa Network First para otras cosas (como APIs externas o la propia navegación)
-workbox.routing.registerRoute(
-  new RegExp('/.*'),
-  new workbox.strategies.NetworkFirst()
-);
-
-self.addEventListener('install', (event) => {
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', (event) => {
-  clients.claim();
-});
